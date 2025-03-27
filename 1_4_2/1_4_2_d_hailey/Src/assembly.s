42038348143ba82ad93792d540564c16e8c497b2
hthi0702 <hthi0702@uni.sydney.edu.au>
2025-03-27 11:24:27 +1100

    improving 1_4_2_d_hailey

--- 1_4_2/1_4_2_d_hailey/Src/assembly.s
+++ 1_4_2/1_4_2_d_hailey/Src/assembly.s
@@ -11,14 +11,13 @@
 .data
 @ define variables
 ascii_string: .asciz "hello!\0" @ Define a null-terminated string
-vowel_array: .asciz "aoeiu" @define vowels
+vowel_array: .asciz "aoeiu" @define vowel array
 
 .text
 
-@ this is the entry function called from the startup file
 main:
 
-	LDR R0, =ascii_string  @ the address of the string
+	LDR R0, =ascii_string  @ load the address of the string
 	LDR R2, =0x00 	@ counter to the current place in the string
 	LDR R6, =0 @counter to vowels detected
 	LDR R9, =0 @counter for letters (e.g. doesnt count punctuation characters)
@@ -27,29 +26,30 @@ string_loop:
 
 	LDRB R3, [R0, R2]	@ load the byte from the ascii_string (byte number R2)
 	STRB R3, [R1, R2]	@ store the byte in the string_buffer (byte number R2)
-	CMP R3, #0	@ Test to see whether this byte is zero (for null terminated)
+	CMP R3, #0	@ Test to see whether this byte is the null terminated character
 	BEQ finished_string  @ if it was null (0) then jump out of the loop
-	ORR R3, R3, #32	@convert letter to lowercase
-	LDR R5, =vowel_array
-	BL letter_detecting @check if a letter
-	ADD R2, #1  @ increment the offset R2
+	ORR R3, R3, #32	@convert character to lowercase
+	LDR R5, =vowel_array @load address of vowel array
+	BL letter_detecting @check if character is a letter
+	ADD R2, #1  @ increment the current place in string offset
 
-	B string_loop  @ loop to the next byte
+	B string_loop  @ loop to the next character
 
-@check if letter above or equal to ascii value for a
+@check if character above or equal to ascii value for a
 letter_detecting:
 	CMP R3, ASCII_a
 	BHS lower_z @if it is check it is below or equal to ascii value for z
 	BX LR @if not back to main loop and moves onto next character
 
+@check if character less than or equal to ascii value for z
 lower_z:
 	CMP R3, ASCII_z
-	BLS vowel_detecting @if it is a letter bracnhes to vowel_detecting
+	BLS vowel_detecting @if it is a letter branches to vowel_detecting
 	BX LR @if not back to main loop and moves onto next character
 
 vowel_detecting:
 
-	LDRB R8, [R5], #1 @load vowel from array and move to next
+	LDRB R8, [R5], #1 @load vowel from array and increment R5 by 1
 	CMP R8, #0 @check not end of vowel array
 	BEQ no_vowel
 	CMP R3, R8 @check if vowel
@@ -59,6 +59,7 @@ vowel_detecting:
 found_vowel:
 	ADD R6, #1
 
+@reset and increment registers before next loop
 no_vowel:
 	MOV R8, #0  @ Clear R8
 	ADD R9, #1 @if between a-z increase R9 - increasing letter count
